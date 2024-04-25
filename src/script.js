b5a53c8c03e3662e44f7f64e7d1cd5204a70d1f6
mohamedocx <karuki.fbi@gmail.com>
2024-04-26 00:19:05 +0200

    first commit

---
+++ src/script.js
@@ -0,0 +1,7 @@
+const mobilebtn=document.querySelector("#mopileBtn");
+const mopilemenu=document.querySelector("#mopilemenu");
+
+
+mobilebtn.addEventListener('click',()=>{
+    mopilemenu.classList.toggle("hidden")
+});
